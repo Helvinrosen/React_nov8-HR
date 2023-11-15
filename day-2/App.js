504756b26effd2afa36e4631916ea4bda999c937
Helvinrosen <124854797+Helvinrosen@users.noreply.github.com>
2023-11-15 15:25:28 +0530

    Add files via upload

---
+++ day-2/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Styling from './component/Styling';
+
+function App() {
+  return (
+    <div className="App">
+      <Styling/>
+    </div>
+  );
+}
+
+export default App;
