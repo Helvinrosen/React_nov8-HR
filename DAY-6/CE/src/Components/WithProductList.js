a8b7c645259921de64123174e2340c6f7e05d563
Helvinrosen <124854797+Helvinrosen@users.noreply.github.com>
2023-11-23 14:48:56 +0530

    Delete DAY-6/CE/src directory

--- DAY-6/CE/src/Components/WithProductList.js
+++
@@ -1,9 +0,0 @@
-// WithProductList.js
-
-import React from 'react';
-
-export function WithProductList(WrappedComponent, products) {
-  return function WithProductListWrapper(props) {
-    return <WrappedComponent products={products} {...props} />;
-  };
-}
